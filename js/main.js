d5c93f1dae23fb6162f7826f2b761c3e68c7bd10
guzmanpdro <pedroguzman2598@gmail.com>
2022-03-12 21:19:32 -0400

    adds new features

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 const hamburgerBtn = document.querySelector('#hamburgerMenu')
 const navBar = document.querySelector('#navbar ul')
+const form = document.querySelector('#form')
 
 hamburgerBtn.addEventListener('click', () => {
   navBar.classList.toggle('show')
 })
+
+form.addEventListener('submit', validationForm)
+
+function validationForm(event) {
+  event.preventDefault()
+  const name = document.querySelector('#name').value
+
+  alert(`Hola mi pana ${name}, a día de hoy el formulario no está funcionando, lo lamento, gracias por interesarte en comunicarte conmigo.`)
+}
